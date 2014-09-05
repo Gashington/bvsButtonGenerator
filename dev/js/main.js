6d23a86b58f86a6ab31678abc50b0b7474d25e12
Gashington <gashington@gmail.com>
2014-09-05 17:40:09 +0300

    finished font block

--- dev/js/main.js
+++ dev/js/main.js
@@ -14,10 +14,7 @@ $(function () {
                 'font-size': '16',
                 'font-family': 'Arial',
                 'text-transform': 'none',
-                'textShadow-color': '#fff',
-                'textShadow-posx': 0,
-                'textShadow-posy': 0,
-                'textShadow-blur': 0,
+                'text-shadow': 'none',
                 //'text-shadow': app.var.default.textShadow-posx
             },
 
@@ -25,7 +22,7 @@ $(function () {
 
             //все текстовые инпуты с которыми работаем
             $input_text: $('.input-text'),
-
+            $shodowText_color: $('#shadowText-color'),
             // набор бегунки (jquery ui slider)
             $sliders: $('.slider'),
             $font_size: $('#font-size'),
@@ -41,44 +38,42 @@ $(function () {
         },
 
 
-
-
-        init: function() {
+        init: function () {
             //console.log(app.var.$input_text);
             // инициализируем бегунки
             app.var.$font_size.slider({
-                range: "min",min: 6,max: 60,step: 1,
+                range: "min", min: 6, max: 60, step: 1,
                 value: app.var.default['font-size'],
-                create: function( event, ui ) {
+                create: function (event, ui) {
                     console.log(ui.value)
-                    $( ".ui-slider-handle", $(this) ).html( $(this).slider('value') + 'px' );
+                    $(".ui-slider-handle", $(this)).html($(this).slider('value') + 'px');
                 }
             });
             app.var.$shadowTextOffset_x.slider({
                 disabled: true,
-                range: "min",min: 0,max: 60,step: 1,
+                range: "min", min: -60, max: 60, step: 1,
                 value: app.var.default['textShadow-posx'],
-                create: function( event, ui ) {
+                create: function (event, ui) {
                     console.log(ui.value)
-                    $( ".ui-slider-handle", $(this) ).html( $(this).slider('value') + 'px' );
+                    $(".ui-slider-handle", $(this)).html($(this).slider('value') + 'px');
                 }
             });
             app.var.$shadowTextOffset_y.slider({
                 disabled: true,
-                range: "min",min: 0,max: 60,step: 1,
+                range: "min", min: -60, max: 60, step: 1,
                 value: app.var.default['textShadow-posy'],
-                create: function( event, ui ) {
+                create: function (event, ui) {
                     console.log(ui.value)
-                    $( ".ui-slider-handle", $(this) ).html( $(this).slider('value') + 'px' );
+                    $(".ui-slider-handle", $(this)).html($(this).slider('value') + 'px');
                 }
             });
             app.var.$shadowText_blur.slider({
                 disabled: true,
-                range: "min",min: 0,max: 60,step: 1,
+                range: "min", min: 0, max: 10, step: 1,
                 value: app.var.default['textShadow-blur'],
-                create: function( event, ui ) {
+                create: function (event, ui) {
                     console.log(ui.value)
-                    $( ".ui-slider-handle", $(this) ).html( $(this).slider('value') + 'px' );
+                    $(".ui-slider-handle", $(this)).html($(this).slider('value') + 'px');
                 }
             });
 
@@ -88,13 +83,19 @@ $(function () {
             app.var.$btn.css(app.var.default)
             app.listeners();
         },
-        listeners: function(){
+        listeners: function () {
             app.var.$input_text.on('keyup change', this.set_input_text_data);
-            app.var.$sliders.on('slide',this.set_slider_text_data)
-            app.var.$textShadow_adv.on('change',this.set_textShadow_adv)
+            app.var.$sliders.on('slide', this.set_slider_text_data);
+            app.var.$sliders.on('slide slidestop', this.sliderHandle_update);
+            app.var.$textShadow_adv.on('change', this.set_textShadow_adv);
+            app.var.$shodowText_color.on('change', this.set_textShadow);
+            $(".slider--adv", app.var.$textShadow_adv__block).on('slide', this.set_textShadow);
+            $(".slider--adv", app.var.$textShadow_adv__block).on('slide slidestop', this.sliderHandle_update);
         },
-
-        set_input_text_data: function(e) {
+        sliderHandle_update: function(e, ui) {
+            $(".ui-slider-handle", $(this)).html($(this).slider('value') + 'px');
+        },
+        set_input_text_data: function (e) {
             var attr = $(this).attr('data-attr');
             if ($(this).val() == '') {
                 app.var.cur_data[attr] = app.var.default[attr];
@@ -107,68 +108,87 @@ $(function () {
             app.var.$btn.css(app.var.cur_data)
         },
 
-        set_slider_text_data: function(e, ui) {
+        set_slider_text_data: function (e, ui) {
             var attr = $(this).attr('data-attr');
-            $( ".ui-slider-handle", $(this) ).html( ui.value + 'px' );
+            //$(".ui-slider-handle", $(this)).html(ui.value + 'px');
             app.var.cur_data[attr] = ui.value;
             app.get_css();
             app.var.$btn.css(app.var.cur_data)
         },
 
-        set_textShadow_adv: function(e) {
-            var textShadow = { 'text-shadow': '5px 5px 5px #fff' }
+        set_textShadow_adv: function (e) {
+            var css = { 'text-shadow': '' }
             if ($(this).prop('checked')) {
-                $("input",app.var.$textShadow_adv__block).prop('disabled', false);
-                $(".slider",app.var.$textShadow_adv__block).slider('enable');
-                //textShadow['text-shadow']+= '5px 5px 5px #ffff';
-                console.log(textShadow)
+                $("input", app.var.$textShadow_adv__block).prop('disabled', false);
+                $(".slider--adv", app.var.$textShadow_adv__block).slider('enable');
+                app.set_textShadow();
             }
             else {
-                $("input",app.var.$textShadow_adv__block).prop('disabled', true);
-                $(".slider",app.var.$textShadow_adv__block).slider('disable');
-                css['text-shadow']+= 'none'
+                $("input", app.var.$textShadow_adv__block).prop('disabled', true);
+                $(".slider--adv", app.var.$textShadow_adv__block).slider('disable');
+                //css['text-shadow'] = 'none';
+                app.var.cur_data['text-shadow'] = 'none'
+                app.var.$btn.css(app.var.cur_data);
+                app.get_css();
             }
-            console.log(app.var.$btn)
-            app.var.$btn.css(textShadow);
-            console.log(app.var.$btn)
+
         },
 
-        get_html: function(){
+        set_textShadow: function (ui) {
+            var css = { 'text-shadow': '' },
+                data = {
+                    color: app.var.$shodowText_color.val(),
+                    posx: app.var.$shadowTextOffset_x.slider('value') + 'px ',
+                    posy: app.var.$shadowTextOffset_y.slider('value') + 'px ',
+                    blur: app.var.$shadowText_blur.slider('value') + 'px '
+                }
+            app.var.cur_data['text-shadow'] = data.posx + data.posy + data.blur+ data.color;
+            //console.log(css);
+            //console.log(app.var.cur_data)
+            app.var.$btn.css(app.var.cur_data);
+            app.get_css();
+        },
+
+        get_html: function () {
             var val = app.var.cur_data['btn-text'];
             app.var.$btn.text(val);
-            $('#html-code').text('<button class="'+ app.var.cur_data['btn-class'] +'">' + app.var.cur_data['btn-text'] + '</button>');
+            $('#html-code').text('<button class="' + app.var.cur_data['btn-class'] + '">' + app.var.cur_data['btn-text'] + '</button>');
             Prism.highlightAll();
         },
 
         //получить готовый css
-        get_css: function(){
+        get_css: function () {
             var data = app.var.cur_data;
             var str = '.' + data['btn-class'] + "{\n";
             str += "\t" + 'color: ' + data['color'] + ";\n";
-            if(data['font-style'] !== 'normal') {
+            if (data['font-style'] !== 'normal') {
                 str += "\t" + 'font-style: ' + data['font-style'] + ";\n";
             }
-            if(data['font-weight'] !== 'normal') {
+            if (data['font-weight'] !== 'normal') {
                 str += "\t" + 'font-weight: ' + data['font-weight'] + ";\n";
             }
             str += "\t" + 'font-size: ' + data['font-size'] + "px;\n";
             str += "\t" + 'font-family: ' + data['font-family'] + ";\n";
-            if(data['text-transform'] !== 'none') {
+            if (data['text-transform'] !== 'none') {
                 str += "\t" + 'text-transform: ' + data['text-transform'] + ";\n";
+            }
+            if (data['text-shadow'] !== 'none') {
+                str += "\t" + 'text-shadow: ' + data['text-shadow'] + ";\n";
             }
 
             str += '}';
             $('#css-code').text(str);
             Prism.highlightAll();
         },
-        cloneObj: function (obj){
-            if(obj == null || typeof(obj) != 'object')
+        cloneObj: function (obj) {
+            if (obj == null || typeof(obj) != 'object')
                 return obj;
             var newObj = {};
             for (var k in obj) {
                 newObj[k] = obj[k];
             }
-            return newObj        }
+            return newObj
+        }
     }
     app.init()
 }())
